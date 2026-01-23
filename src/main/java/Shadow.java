982d0f3f218327b1223c83af5a73384715fc9848
Ash Chen <94738775+Ash-L7@users.noreply.github.com>
2026-01-23 11:13:05 +0800

    Update echo

--- src/main/java/Shadow.java
+++ src/main/java/Shadow.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Shadow {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo = "";
-        System.out.println("Hello! I'm Shadow\n" +
-                "What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("- Hello! I'm Shadow\n" +
+                "- What can I do for you?");
+
+        String userInput = scanner.nextLine();
+
+        while (!userInput.equals("Bye")) {
+            System.out.println("- " + userInput);
+            userInput = scanner.nextLine();
+        }
+
+        System.out.println("- Bye. Hope to see you again soon!\n");
     }
 }
