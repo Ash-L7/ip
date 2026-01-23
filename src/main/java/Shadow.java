625fb0404abc959c578efb929bc2b81c6c234aa0
Ash Chen <94738775+Ash-L7@users.noreply.github.com>
2026-01-23 11:00:11 +0800

    Update chatbot name and greetings

---
+++ src/main/java/Shadow.java
@@ -0,0 +1,8 @@
+public class Shadow {
+    public static void main(String[] args) {
+        String logo = "";
+        System.out.println("Hello! I'm Shadow\n" +
+                "What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
