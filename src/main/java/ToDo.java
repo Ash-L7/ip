84f458e95f2019516a9b920d65ef3e115fb6c12e
Ash Chen <94738775+Ash-L7@users.noreply.github.com>
2026-02-12 19:04:39 +0800

    Add header comments for ToDo class

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,4 +1,16 @@
+/**
+ * ToDo is a subclass of Task with a [T] marker.
+ * It represents the most basic kind of task
+ * with no additional date or time fields
+ * and only records the description of the task
+ */
 public class ToDo extends Task {
+    /**
+     * The constructor for ToDo Task object.
+     * Initializes isDone and marks task as incomplete by default.
+     *
+     * @param name Description of the task being created.
+     */
     public ToDo(String name) {
         super(name);
     }
