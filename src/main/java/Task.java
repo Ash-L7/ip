865af049c78ce7b4bf8732558c352c4f8b8f794b
Ash Chen <94738775+Ash-L7@users.noreply.github.com>
2026-02-12 19:00:04 +0800

    Add header comments for Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,16 +1,29 @@
+/**
+ * The representation of a task,
+ * Records the description and completion status of the task.
+ */
 public class Task {
-    private String name;
+    /** The description of the task. */
+    private final String name;
+    /** Keeps track of whether task has been completed. */
     private boolean isDone;
 
+    /**
+     * The constructor for Task object.
+     * Initializes isDone and marks task as incomplete by default.
+     *
+     * @param name Description of the task being created.
+     */
     public Task(String name) {
         this.name = name;
         this.isDone = false;
     }
 
-    public String getName() {
-        return this.name;
-    }
-
+    /**
+     * Checks isDone field and returns whether the task is completed.
+     *
+     * @return A String representation of whether a task is completed.
+     */
     public String getIsDone() {
         if (this.isDone) {
             return "X";
@@ -19,6 +32,13 @@ public class Task {
         return " ";
     }
 
+    /**
+     * Marks a task as complete or incomplete base on user input.
+     * Displays a string representation of the task being marked
+     * and a string prompt to tell user whether the task is being marked as complete or incomplete.
+     *
+     * @param status Marks task as complete or incomplete.
+     */
     public void setDone(boolean status) {
         if (status) {
             this.isDone = true;
