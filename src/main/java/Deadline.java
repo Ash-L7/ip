6a8dab46c16fc7fd0a37007a9617bcda63bebe1d
Ash Chen <94738775+Ash-L7@users.noreply.github.com>
2026-02-12 19:08:37 +0800

    Add header comments for Deadline class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,6 +1,18 @@
+/**
+ * Deadline is a subclass of Task with a [D] marker.
+ * It represents task with a due date.
+ */
 public class Deadline extends Task{
-    private String deadline;
+    /** Due day of the task */
+    private final String deadline;
 
+    /**
+     * The constructor for Deadline Task object.
+     * Takes in the description and the due day of the task.
+     *
+     * @param name Description of the task being created.
+     * @param deadline A string of a day in a week representing the due date of the task.
+     */
     public Deadline(String name, String deadline) {
         super(name);
         this.deadline = deadline;
