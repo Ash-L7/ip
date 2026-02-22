26da8235697d23ee317826bad5b7756ac99b87ae
Ash Chen <94738775+Ash-L7@users.noreply.github.com>
2026-02-22 18:59:48 +0800

    Update InvalidTaskDescriptionException.java

--- src/main/java/shadow/exception/InvalidCommandException.java
+++ src/main/java/shadow/exception/InvalidCommandException.java
@@ -3,7 +3,7 @@ package shadow.exception;
 public class InvalidCommandException extends ShadowException {
     /**
      * Creates a new shadow.exception.ShadowException with the specified error message when a non-existing command is
-     * entered
+     * entered.
      *
      * @param message The error message describing what went wrong
      */
--- src/main/java/shadow/exception/InvalidTaskDescriptionException.java
+++ src/main/java/shadow/exception/InvalidTaskDescriptionException.java
@@ -1,6 +1,12 @@
 package shadow.exception;
 
 public class InvalidTaskDescriptionException extends ShadowException {
+    /**
+     * Creates a new shadow.exception.ShadowException with the specified error message when no task description is
+     * given.
+     *
+     * @param message The error message describing what went wrong
+     */
     public InvalidTaskDescriptionException(String message) {
         super(message);
     }
