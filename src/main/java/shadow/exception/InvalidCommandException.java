eed1d9650191d21494a617497b0b34784c93ea08
Ash Chen <94738775+Ash-L7@users.noreply.github.com>
2026-02-22 18:58:30 +0800

    Update InvalidCommandException.java

--- src/main/java/shadow/exception/InvalidCommandException.java
+++ src/main/java/shadow/exception/InvalidCommandException.java
@@ -2,7 +2,8 @@ package shadow.exception;
 
 public class InvalidCommandException extends ShadowException {
     /**
-     * Creates a new shadow.exception.ShadowException with the specified error message
+     * Creates a new shadow.exception.ShadowException with the specified error message when a non-existing command is
+     * entered
      *
      * @param message The error message describing what went wrong
      */
