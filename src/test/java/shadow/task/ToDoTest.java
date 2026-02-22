1115913267f2beb97689876e553780ac87521a1b
Ash Chen <94738775+Ash-L7@users.noreply.github.com>
2026-02-22 18:25:08 +0800

    Add ToDoTest.java

--- src/test/java/shadow/task/ToDoTest.java
+++ src/test/java/shadow/task/ToDoTest.java
@@ -1,4 +1,17 @@
 package shadow.task;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ToDoTest {
+    @Test
+    public void testFileFormatConversion() {
+        assertEquals("T, ,read book", new ToDo("read book").toFileFormat());
+    }
+
+    @Test
+    public void testStringConversion() {
+        assertEquals("[T][ ] clean room", new ToDo("clean room").toString());
+    }
 }
